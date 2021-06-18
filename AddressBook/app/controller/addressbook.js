7baff03aa01452b0ef71252e32afd1aebab81fc4
Kajal <kajalpatil158@gmail.com>
2021-06-18 17:27:42 +0530

    feat : validation is added in models

---
+++ AddressBook/app/controller/addressbook.js
@@ -0,0 +1,1 @@
+const AddressBook = require('../models/addressbook.js');
---
+++ AddressBook/models/addressbook.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const AddressBookSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        validate: /^[A-Z]{1}[a-z]{2,}$/
+    },
+    lastName: {
+        type: String,
+        required: true,
+        validate: /^[A-Z]{1}[a-z]{2,}$/
+    },
+    address: {
+        type: String,
+        required: true,
+        validate: /^[A-Z]{1}[a-z]{2,}$/
+    },
+    city: {
+        type: String,
+        required: true,
+        validate: /^[A-Z]{1}[a-z]{2,}$/
+    },
+    state: {
+        type: String,
+        required: true,
+        validate: /^[A-Z]{1}[a-z]{2,}$/
+    },
+    zip: {
+        type: String,
+        required: true,
+        validate: /^[A-Z]{1}[a-z]{2,}$/
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        validate: /^[A-Z]{1}[a-z]{2,}$/
+    },
+    emailId: {
+        type: String,
+        required: true,
+        validate: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('AddressBook', AddressBookSchema);
