4d2259750b1a2460289ce5fcb28099c4e1c70cdb
Kajal <kajalpatil158@gmail.com>
2021-06-22 21:53:18 +0530

    feat: user service code is added

---
+++ AddressBook/app/service/userData.js
@@ -0,0 +1,15 @@
+const UserModel = require('../models/userData.js');
+
+class UserService {
+    /* @Description - create method is created.
+     * @param- addressBookData send from controller
+     * @return callback is used to callback controller
+     */
+    create = (userData, callBack) => {
+        UserModel.create(userData, (error, data) => {
+            return (error) ? callBack(error, null) : callBack(null, data);
+        });
+    }
+}
+
+module.exports = new UserService();
